759d8a390622b846c1d3b1a7e819e744652e790a
Bishal Gautam <bisalgt@gmail.com>
2020-05-06 18:34:12 +0545

    unittest implemented

--- chapter1/function_tests.py
+++ chapter1/function_tests.py
@@ -1,5 +1,38 @@
 from selenium import webdriver
 
+
 browser = webdriver.Firefox()
+
+
+# Edith heard about the cool to-do app and
+# goes tocheckout its homepage
 browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+
+# she notices page title and header mention to-do lists
+assert 'To-Do' in browser.title, "Browser title was "+ browser.title
+
+
+# she is invited to enter a to-do item straight away
+
+
+# she types 'Buy peacock feathers' into a text box
+
+# she hits enters the page updates and now the page lists
+# "1. Buy Peacock feathers" as a item in a to-do list
+
+
+# There is still a text box inviting her to add another item.
+# she enters "Use peacock feathers to make a fly"
+
+
+# The page updates and now show both items on her lists
+
+# Edith wonders wether the site remembers her lists.
+# she sees the site has generated a unique url for her -- 
+
+# she visits the url-- her todo list is still there
+
+# Satisfied, she goes to sleep
+
+browser.quit()
+
---
+++ chapter1/functional_tests.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Visit the homepage
+        self.browser.get('http://localhost:8000')
+        
+        # Notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+
+
+        # invited to add todo item
+
+
+if __name__=='__main__':
+    unittest.main()
+
