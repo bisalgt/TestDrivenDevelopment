7aed6a88648b8338ad9f5f242bff1dbd7ed20175
Bishal Gautam <bisalgt@gmail.com>
2020-05-07 09:55:24 +0545

    manually rendenring the template and comparing to what view returns

--- chapter1/lists/tests.py
+++ chapter1/lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
@@ -14,6 +15,6 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        # print (repr(html))
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
