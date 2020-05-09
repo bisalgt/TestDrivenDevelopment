ee839ae274dc32977a03b8c93cbeb8ee2d2a8ed7
Bishal Gautam <bisalgt@gmail.com>
2020-05-09 09:13:20 +0545

    redirect after post and show all items in template

--- chapter1/functional_tests.py
+++ chapter1/functional_tests.py
@@ -24,6 +24,7 @@ class NewVisitorTest(unittest.TestCase):
         # Notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
+        time.sleep(10)
         self.assertIn('To-Do', header_text)
 
         # Invited to enter a todo item straight away
@@ -39,7 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         rows =  table.find_elements_by_tag_name('tr')
         inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(10)
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
 
@@ -51,14 +52,6 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-
-
-
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows], f'Not found in {table.text}')     
-        
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows], f'Not found in {table.text}')     
-
-
 
 
         self.fail('Finish the test')
