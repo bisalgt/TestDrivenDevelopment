b8cd5010e4aeca7b2cc5ace3bf281bb7d19dc8ac
Bishal Gautam <bisalgt@gmail.com>
2020-06-29 23:12:22 +0545

    functional test using selenium

--- tdd_project/functional_tests.py
+++ tdd_project/functional_tests.py
@@ -1,10 +1,12 @@
+#! /home/bishal/TestDrivenDevelopment/tdd_project/env/bin/python3
+
 from selenium import webdriver
 
 browser = webdriver.Firefox()
 browser.get("http://localhost:8000")
 # browser.sleep(4)
 
-assert 'Django' in browser.title
+assert 'TO-DO' in browser.title, "Browser title was "+ browser.title
 
 browser.quit()
 
